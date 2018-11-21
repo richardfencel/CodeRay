af47dc72d83bc713b5296bfd4ddc01e3b4ae17e7
Richard Fencel <richardfen@cox.net>
2018-11-20 23:10:49 -0800

    Version 2

--- SimpleListBoxWpfApp/App.xaml.cs
+++ SimpleListBoxWpfApp/App.xaml.cs
@@ -5,11 +5,6 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-using CodeRayWpfLib;
-
-// Copyright Richard Fencel Software (c) 2014
-using CodeRayTestSimpleListBoxWpfApp.ViewModels;
-using CodeRayTestSimpleListBoxWpfApp.Views;
 
 namespace SimpleListBoxWpfApp
 {
@@ -18,17 +13,5 @@ namespace SimpleListBoxWpfApp
     /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-           
-            var viewModel = new MainWindowViewModel(null);
-
-            var window = new MainWindowView();
-            window.DataContext = viewModel;
-            window.Show();
-
-            CodeRayDlg.CodeRayShow();
-        }
     }
 }
